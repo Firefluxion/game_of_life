81281045f88575deb08ade1309d96901c3856b42
kekobleibtkeko <60101566+kekobleibtkeko@users.noreply.github.com>
2020-06-22 17:15:26 +0200

    Add files via upload
    add comments for headers
    add programm description

--- GoL.h
+++ GoL.h
@@ -1,9 +1,21 @@
 #ifndef GOL_H_INCLUDED
 #define GOL_H_INCLUDED
 
+/*
+    This header contains the needed data structures and functions regarding them
+*/
 #include "GoL.Structs.h"
+/*
+    This header contains constants needed for the other headers
+*/
 #include "GoL.Constants.h"
+/*
+    This header contains all code relating to the simulation of fields
+*/
 #include "GoL.Runtime.h"
+/*
+    This header contains the methods for input/ output operations
+*/
 #include "GoL.IO.h"
 
-#endif // GOL_H_INCLUDED
+#endif
--- main.c
+++ main.c
@@ -1,4 +1,21 @@
+/**
+
+    The "Game of Life"
+    Original concept by John Horton Conway
+
+    Authors: Alexander Thielker & Robin Gerwe
+    Date: 22.06.2020
+
+    The Game of Life is a cellular automaton. It is a "zero-player game", and is made out of a potentially
+    infinite grid of cells. These cells can be either "dead" or "alive". A cell may die or be revived
+    depending on the amount of living neighbors.
+    The initial pattern is determined randomly.
+    (https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life)
+    (https://github.com/Firefluxion/game_of_life)
+**/
+
 #include "GoL.h"
+
 
 /*
     This is the starting point for the game of live program.
