9e96b458aa897557885b9109b7ab8e6aa05c9627
kekobleibtkeko <60101566+kekobleibtkeko@users.noreply.github.com>
2020-06-22 15:52:10 +0200

    Add files via upload
    add comments

--- GoL.Runtime.c
+++ GoL.Runtime.c
@@ -13,24 +13,30 @@
 #include "GoL.Runtime.h"
 
 int dead_min = 2;
-int dead_max_allowed = 3;
+int dead_max = 3;
 
 int alive_min = 2;
 int alive_max = 4;
 
+
+/*
+    Main method for a simulation. Reads user input and displays the iterations
+*/
 int run_field(struct field* field)
 {
     double simulationsPerSecond = 3.0;
     int automatic = 1, finished = 0, i = 0;
-    char c;
-    struct field* buffer[BUFFERSIZE]; //Kommentar
+    char c = 0;
 
+    //Buffer keeps track of the last n iterations, also compares them to the current one to
+    //see if a stable condition has been reached
+    struct field* buffer[BUFFERSIZE];
+
+    //Buffer is filled with the current iteration to avoid null references
     for (i = 0; i < BUFFERSIZE; i++)
     {
         buffer[i] = copy_field(field);
     }
-
-    c = 0;
     clear();
 
     do
@@ -38,46 +44,61 @@ int run_field(struct field* field)
         if (automatic)
         {
             usleep(1000000 / simulationsPerSecond);
+            //Detects id the keyboard was pressed without halting execution
             if (kbhit())
             {
+                c = getch();
+                fflush(stdin);
+
                 automatic = 0;
+
                 clear();
+
                 print_field(field);
                 print_info(field, automatic, simulationsPerSecond, field->iteration, finished);
-                fflush(stdin);
                 continue;
             }
         }
         else
         {
-            switch(getch())
+            c = getch();
+            //Manual mode; iteration is stopped until the user inputs a key, the info will display the possible actions.
+            //Any key will forward the simulation, but some have additional functions
+            switch(c)
             {
+                // 'a' switches the simulation back to automatic mode
             case 'a':
                 automatic = 1;
-                fflush(stdin);
+                clear();
                 break;
 
+                // 's' saves the current iteration to a file
             case 's':
                 write_field_to_file(field, "Test.txt");
                 printf("File saved.\n");
                 continue;
 
+                // 'r' reads a saved simulation
             case 'r':
                 field = read_field_from_file("Test.txt");
+                //The buffer is filled with an empty field to make sure the simulation doesn't stop due to a wrong stable condition
                 struct field* empty = new_field(field->width, field->height, ' ', 0);
                 for (i = 0; i < BUFFERSIZE; i++)
                 {
-                    buffer[i] = empty;
+                    buffer[i] = copy_field(empty);
                 }
                 clear();
+
                 print_field(field);
                 print_info(field, automatic, simulationsPerSecond, field->iteration, finished);
                 continue;
 
+                // 'o' gives the user some inputs for the ruleset
             case 'o':
                 set_options();
                 continue;
 
+                // 'i' allows the user to change the automatic speed
             case 'i':
                 printf("Please enter the desired iteration speed.\n");
                 simulationsPerSecond = safe_number_input(0, 90);
@@ -85,15 +106,18 @@ int run_field(struct field* field)
             }
         }
 
+        //the current field is copied to the buffer. the modulo makes sure the assigned index stays within the bounds of the array.
         buffer[field->iteration % BUFFERSIZE] = copy_field(field);
         simulate_field(field);
 
         for (i = 0; i < BUFFERSIZE; i++)
         {
+            //the newly simulated field is compared to the buffered values.
             if (compare_fields(buffer[i], field))
             {
                 finished = 1;
                 clear();
+                //iteration is decremented, since no change has occurred since the last iteration.
                 field->iteration--;
             }
         }
@@ -101,6 +125,7 @@ int run_field(struct field* field)
         print_field(field);
         print_info(field, automatic, simulationsPerSecond, field->iteration, finished);
     }
+    // 'x' exits the simulation.
     while (c != 'x' && finished == 0);
 
     printf("Simulation has completed.");
@@ -108,10 +133,13 @@ int run_field(struct field* field)
     return 0;
 }
 
+/*
+    Simulates the changes in the field for one iteration step with the given ruleset
+*/
 void simulate_field(struct field* field)
 {
     field->iteration++;
-    int amount = field->width * field->height;
+    int amount = get_field_cell_count(field);
     struct cell* newCells = new_cell_list(field);
     int i = 0;
     for (i = 0; i < amount; i++)
@@ -121,6 +149,9 @@ void simulate_field(struct field* field)
     field->cells = newCells;
 }
 
+/*
+    Prints the field data itself (including the frame)
+*/
 void print_field(struct field* field)
 {
     set_cursor(0, 0);
@@ -150,17 +181,20 @@ void print_field(struct field* field)
     set_cursor(0, field->height + 2);
 };
 
+
+/*
+    Prints the field data utilizing the windows API, currently unused due to problems with small field sizes (e.g. (7, 7))
+*/
 void print_field_win(struct field* field)
 {
     struct cell c;
 
     HANDLE stdOut = GetStdHandle(STD_OUTPUT_HANDLE);
     DWORD written = 0;
-    const char *message = "hello world";
+    const char *message = "";
     if (stdOut != NULL && stdOut != INVALID_HANDLE_VALUE)
     {
 
-    //    WriteConsoleA(stdOut, message, strlen(message), &written, NULL);
     }
 
     set_cursor(0, 0);
@@ -191,33 +225,51 @@ void print_field_win(struct field* field)
     set_cursor(0, field->height + 2);
 }
 
+/*
+    Prints the field info next to the field itself
+*/
 void print_info(struct field* field, int automatic, double iterations, int currentIteration, int finished)
 {
+    int xDistanceFromField = 3;
     if (!finished)
     {
         if (automatic)
         {
-            set_cursor(field->width + 2, 1);
+            set_cursor(field->width + xDistanceFromField, 3);
             printf("Simulation is running in auto-mode, press any button to pause.");
-            set_cursor(field->width + 2, 3);
+            set_cursor(field->width + xDistanceFromField, 5);
             printf("Current iterations per second: %lf", iterations);
         }
         else
         {
-            set_cursor(field->width + 2, 1);
-            printf("Simulation is in manual mode, enter 'a' to return to auto-mode.");
+            set_cursor(field->width + xDistanceFromField, 3);
+            printf("Simulation is in manual mode, press 'a' to return to auto-mode.");
+            set_cursor(field->width + xDistanceFromField, 5);
+            printf("Press 's' to save the current iteration.");
+            set_cursor(field->width + xDistanceFromField, 7);
+            printf("Press 'r' to read a saved simulation.");
+            set_cursor(field->width + xDistanceFromField, 9);
+            printf("Press 'i' to change the automatic iteration speed.");
+            set_cursor(field->width + xDistanceFromField, 11);
+            printf("Press 'o' to change the ruleset.");
+            set_cursor(field->width + xDistanceFromField, 13);
+            printf("Press 'x' to end the current simulation.");
         }
     }
     else
     {
-        set_cursor(field->width + 2, 1);
+        set_cursor(field->width + xDistanceFromField, 1);
         printf("Simulation has reached a stable condition.");
     }
-    set_cursor(field->width + 2, 5);
+    set_cursor(field->width + xDistanceFromField, 1);
     printf("Current iteration: %d", currentIteration);
 
     set_cursor(0, field->height + 2);
 }
+
+/*
+    The cell is simulated by getting its neighbors and checking against the rule set.
+*/
 struct cell simulate_cell(struct field* field, struct cell cell)
 {
     struct cell* neighbors = get_neighbors(field, cell.position);
@@ -234,7 +286,7 @@ struct cell simulate_cell(struct field* field, struct cell cell)
     switch (cell.state)
     {
     case alive:
-        if (aliveCount < dead_min || aliveCount > dead_max_allowed)
+        if (aliveCount < dead_min || aliveCount > dead_max)
         {
             cell.state = dead;
         }
@@ -249,6 +301,10 @@ struct cell simulate_cell(struct field* field, struct cell cell)
 
     return cell;
 };
+
+/*
+    Returns the minimum value if the input is greater than the max value (and vice versa), otherwise returns the input
+*/
 int wrap_around(int n, int min, int max)
 {
     if (n < min)
@@ -262,6 +318,10 @@ int wrap_around(int n, int min, int max)
     return n;
 }
 
+/*
+    Code by Christian Wichman in the project "Erweiterte Funktionen der Konsole" (https://moodle.nibis.de/bbs_osb/mod/forum/discuss.php?d=1177)
+    Prints an ASCII frame in the given size starting in the top left.
+*/
 void print_frame(int width, int height)
 {
     int i = 0, j = 0;
@@ -288,6 +348,10 @@ void print_frame(int width, int height)
     }
 }
 
+/*
+    Reads user input for a number, checks whether or not the input is within the set bounds and if it's even a number,
+    of not, the user is prompted to reenter the data.
+*/
 double safe_number_input(double min, double max)
 {
     int valid = 0;
@@ -322,6 +386,10 @@ double safe_number_input(double min, double max)
     return n;
 }
 
+/*
+    Code by Christian Wichman in the project "Erweiterte Funktionen der Konsole" (https://moodle.nibis.de/bbs_osb/mod/forum/discuss.php?d=1177)
+    Sets the cursor to the selected position
+*/
 int set_cursor(int x, int y)
 {
     COORD koordinaten;
@@ -331,20 +399,26 @@ int set_cursor(int x, int y)
     return 0;
 }
 
+/*
+    Allows the user to input values to modify the rule set.
+*/
 void set_options()
 {
-    printf("Please enter a number for the minimum required neighbors for a cell to die.\n");
+    printf("Please enter a number for the minimum required neighbors for a cell to die. (Default: 2)\n");
     dead_min = safe_number_input(0, (double)INT_MAX);
-    printf("Please enter a number for the maximum allowed neighbors for a cell to stay alive.\n");
-    dead_max_allowed = safe_number_input(0, (double)INT_MAX);
+    printf("Please enter a number for the maximum allowed neighbors for a cell to stay alive. (Default: 3)\n");
+    dead_max = safe_number_input(0, (double)INT_MAX);
 
-    printf("Please enter a number for the minimum required neighbors for a cell to revive.\n");
+    printf("Please enter a number for the minimum required neighbors for a cell to revive. (Default: 2)\n");
     alive_min = safe_number_input(0, (double)INT_MAX);
-    printf("Please enter a number for the maximum allowed neighbors for a cell to revive.\n");
+    printf("Please enter a number for the maximum allowed neighbors for a cell to revive. (Default: 4)\n");
     alive_max = safe_number_input(0, (double)INT_MAX);
 }
 
-
+/*
+    Code by StackOverflow user 'nbro' (https://stackoverflow.com/questions/2347770/how-do-you-clear-the-console-screen-in-c)
+    Clears the entire console.
+*/
 void clear(){
     #if defined(__linux__) || defined(__unix__) || defined(__APPLE__)
         system("clear");
--- GoL.Runtime.h
+++ GoL.Runtime.h
@@ -14,23 +14,24 @@
 #ifndef GOL_RUNTIME_H_INCLUDED
 #define GOL_RUNTIME_H_INCLUDED
 
-extern int dead_min;
-extern int dead_max_allowed;
+extern int dead_min; //Minimum required alive neighbors for a cell to die
+extern int dead_max; //Maximum allowed alive neighbors for a cell to stay alive
 
-extern int alive_min;
-extern int alive_max;
+extern int alive_min; //Minimum required alive neighbors for a cell to revive
+extern int alive_max; //Maximum allowed alive neighbors for a cell to stay alive
 
-void print_field(struct field*);
-void print_field_win(struct field*);
-void print_info(struct field* field, int automatic, double iterations, int currentIteration, int finished);
-int run_field(struct field* field);
-int wrap_around(int n, int min, int max);
-struct cell simulate_cell(struct field* field, struct cell cell);
-int set_cursor(int x, int y);
-void print_frame(int, int);
-double safe_number_input(double min, double max);
-void set_options();
-void clear();
+void print_field(struct field*); // prints field data (including frame)
+void print_field_win(struct field*); // prints field data (including frame) utilizing windows API functions (currently unused)
+void print_info(struct field* field, int automatic, double iterations, int currentIteration, int finished); //Prints field infos and controls
+int run_field(struct field* field); //Starts a simulation and guides the user through it
+int wrap_around(int n, int min, int max); //sets a value to max if its less than min and vice versa
+struct cell simulate_cell(struct field* field, struct cell cell); //simulates a single iteration for a cell
+void simulate_field(struct field* field); //simulates a single iteration for a field
+int set_cursor(int x, int y); //sets the console cursor to the designated position
+void print_frame(int, int); //prints a frame in the designated size
+double safe_number_input(double min, double max); //Checks if user input is within bounds and prompts user to reenter if it isn't
+void set_options(); //set rule set
+void clear(); //clears the console
 
 
 
